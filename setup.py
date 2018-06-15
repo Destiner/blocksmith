3dace378970acabcd6022b23384966a3c78f2502
Timur Badretdinov <destinerx@gmail.com>
2018-06-15 21:45:23 +0300

    Add setup-related files

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name = 'blocksmith',
+    packages = ['blocksmith'],
+    version = '1.0',
+    description = 'Bitcoin key manipulation',
+    author = 'Timur Badretdinov',
+    author_email = 'destinerx@gmail.com',
+    url = 'https://github.com/Destiner/blocksmith',
+    download_url = 'https://github.com/Destiner/blocksmith/tarball/1.0',
+    keywords = ['security', 'cryptography', 'cryptocurrency', 'bitcoin'],
+    classifiers = [],
+)
