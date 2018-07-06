7adf6379ad744e504c5a0e8b30e6067231c4f6fb
Timur Badretdinov <destinerx@gmail.com>
2018-07-06 21:37:35 +0300

    Add ecdsa package dependency to setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from setuptools import setup
 setup(
     name = 'blocksmith',
     packages = ['blocksmith'],
-    version = '1.1.1',
+    install_requires = ['ecdsa'],
+    version = '1.1.2',
     description = 'Bitcoin key manipulation',
     author = 'Timur Badretdinov',
     author_email = 'destinerx@gmail.com',
     url = 'https://github.com/Destiner/blocksmith',
-    download_url = 'https://github.com/Destiner/blocksmith/tarball/1.1.1',
+    download_url = 'https://github.com/Destiner/blocksmith/tarball/1.1.2',
     keywords = ['security', 'cryptography', 'cryptocurrency', 'bitcoin'],
     classifiers = [],
 )
